28c0f1a90ce4bb7d9a65573c34d28b1974de6699
broholens <zzwcng@126.com>
2025-12-12 20:49:44 +0800

    fix: 修复找不到app bug

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 SCRIPT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-# TODO: install python & venv
-pip install -r $SCRIPT_PATH/requirements.txt > /dev/null
+#python3 -m venv imageenv
+#source imageenv/bin/activate
+#pip install -r $SCRIPT_PATH/requirements.txt > /dev/null
 
 PORT=9999
 lsof -i:$PORT | grep -v "PID" | awk '{print $2}' | xargs -r kill -9
 sleep 1
 
 cd $SCRIPT_PATH
-nohup uvicorn app:app --reload --workers=4 --host 0.0.0.0 --port $PORT > $SCRIPT_PATH/uvicorn.log 2>&1 &
+nohup uvicorn main:app --reload --workers=4 --host 0.0.0.0 --port $PORT > $SCRIPT_PATH/uvicorn.log 2>&1 &
